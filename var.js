4c4f2e4080fe5f2f6700c41d80c0630bff9592f2
Rolly3 <39523055+Rolly3@users.noreply.github.com>
2018-05-22 18:07:21 +0300

    Add files via upload

---
+++ var.js
@@ -0,0 +1,14 @@
+//используем современный стандарт
+"use strict";
+var admin, name;
+name = 'Vasiliy';
+admin = name; 
+alert(admin);
+
+//упражнение 2
+var planetName;
+planetName = 'Земля';
+var userName;
+userName = 'Петя';
+
+alert(planetName + ' - это ' + userName);
